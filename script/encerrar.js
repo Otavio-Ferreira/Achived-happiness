35acb386aae3bb2d1dbb843ddf880498098d3368
Otavio Ferreira <105357022+Otavio-Ferreira@users.noreply.github.com>
2023-07-07 17:51:53 -0300

    Correção de erro

--- script/encerrar.js
+++ script/encerrar.js
@@ -19,5 +19,27 @@ function encerrarSessao(){
     }
     window.location = `../index.html`
   });
-  
+}
+
+function encerrarSessaoHome(){
+ 
+  swal({
+    title: "Tem certeza que quer encerrar essa sessão?",
+    text: "Você terá que iniciar tudo novamente",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("Sessão encerrada!", {
+        icon: "success",
+      });
+      localStorage.setItem("nome", "")
+
+    } else {
+      swal("A sessão não foi encerrada!");
+    }
+    window.onload() 
+  });
 }
